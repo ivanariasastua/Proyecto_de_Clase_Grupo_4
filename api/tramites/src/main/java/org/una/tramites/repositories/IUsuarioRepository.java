44c5240c11f335e457514726129bfd5546f86290
IvanAriasAstua <ivan.arias.astua@est.una.ac.r>
2020-08-22 11:11:09 -0600

    Se creo la interface para la entidad usuario
    IUsuariosRepository, esto para el manejo de funciones JPA

---
+++ api/tramites/src/main/java/org/una/tramites/repositories/IUsuarioRepository.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.una.tramites.repositories;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.una.tramites.entities.Usuario;
+
+/**
+ *
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+public interface IUsuarioRepository extends JpaRepository<Usuario, Long>{
+    
+    public Usuario findByCedulaAndPasswordEncriptado(String cedula, String passwordEncriptado);
+
+    public List<Usuario> findByCedulaContaining(String cedula);
+
+    public List<Usuario> findByNombreCompletoContainingIgnoreCase(String nombreCompleto);
+    
+    @Query("select u from Usuarios u where UPPER(u.nombreCompleto) like CONCAT('%', UPPER(:nombreCompleto), '%'")
+    public Usuario findNombreCompletoWithLikeSQL(@Param("nombreCompleto")String nombreCompleto);
+
+    @Query("select u from Usuarios u where u.cedula = :cedula")
+    public Usuario findByCedula(@Param("cedula")String cedula);
+}
