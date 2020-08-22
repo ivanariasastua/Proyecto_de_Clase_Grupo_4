25080e6bcaa08f310c674be61e4b59f18993f691
YohellBuzo <54456019+YohellBuzo@users.noreply.github.com>
2020-08-22 10:48:14 -0600

    Paquete de entidades y la entidad de usuario

---
+++ api/tramites/src/main/java/org/una/tramites/entities/Usuario.java
@@ -0,0 +1,78 @@
+package org.una.tramites.entities;
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "usuarios")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Usuario implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nombre_completo", length = 100)
+    private String nombreCompleto;
+
+    @Column(length = 100, name = "password_encriptado")
+    private String passwordEncriptado;
+
+    @Column(length = 25, unique = true)
+    private String cedula;
+
+    @Column
+    private boolean estado;
+
+    @Column(name = "departamento_id")
+    private Long DepartamentoId; 
+    
+    @Column(name = "fecha_registro", updatable = false)
+    @Temporal(TemporalType.DATE)
+    @Setter(AccessLevel.NONE)
+    private Date fechaRegistro;
+
+    @Column(name = "fecha_modificacion")
+    @Setter(AccessLevel.NONE)
+    @Temporal(TemporalType.DATE)
+    private Date fechaModificacion;
+
+    @Column(name = "es_jefe")
+    private boolean esJeFe;
+
+    private static final long serialVersionUID = 1L;
+
+    @PrePersist
+    public void prePersist() {
+        estado=true;
+        esJeFe=false;
+        fechaRegistro = new Date();
+        fechaModificacion = new Date();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        fechaModificacion = new Date();
+    }
+
+}
+
+
