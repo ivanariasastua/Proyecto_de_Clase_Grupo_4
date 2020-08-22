eed22a3b9fb2df44e55cf2b1a3b887a5db76ba3a
YohellBuzo <54456019+YohellBuzo@users.noreply.github.com>
2020-08-22 11:23:29 -0600

    Paquete DTO y usuarios DTO

---
+++ api/tramites/src/main/java/org/una/tramites/dto/UsuarioDTO.java
@@ -0,0 +1,26 @@
+package org.una.tramites.dto;
+
+import java.sql.Date;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor 
+@ToString
+public class UsuarioDTO {
+ 
+    private Long id; 
+    private String nombreCompleto;   
+    private String cedula; 
+    private boolean estado; 
+    private Date fechaRegistro; 
+    private Date fechaModificacion; 
+    private Long departamentoId; 
+    private boolean esJefe;
+    
+    
+}
+
