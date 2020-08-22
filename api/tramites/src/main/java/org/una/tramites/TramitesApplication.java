ee59e1ceb28bf4baadd6f959e37a78fb1d0a9603
AngieCordoba <cordobaangie98@gmail.com>
2020-08-21 19:33:44 -0600

    creación de la carpeta api

---
+++ api/tramites/src/main/java/org/una/tramites/TramitesApplication.java
@@ -0,0 +1,13 @@
+package org.una.tramites;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TramitesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TramitesApplication.class, args);
+	}
+
+}
---
+++ api/tramites/src/test/java/org/una/tramites/TramitesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.una.tramites;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TramitesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
