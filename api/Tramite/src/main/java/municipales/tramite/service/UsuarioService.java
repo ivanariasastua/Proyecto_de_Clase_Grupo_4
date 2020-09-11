82898b900dbfb2665847d43f3d16501aa7bdd002
ivanariasastua <69368731+ivanariasastua@users.noreply.github.com>
2020-09-10 21:55:41 -0600

    Addicion de Usuarios Service y clase Respuesta

---
+++ api/Tramite/src/main/java/municipales/tramite/service/UsuarioService.java
@@ -0,0 +1,172 @@
+package municipales.tramite.service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.ws.rs.core.GenericType;
+import municipales.tramite.dto.UsuarioDTO;
+import municipales.tramite.util.Request;
+import municipales.tramite.util.Respuesta;
+
+/**
+ *
+ * @author ivana
+ */
+public class UsuarioService {
+    
+    public Respuesta LogIn(String userName, String userPassword){
+        return null;
+    }
+    
+    public Respuesta getAll(){
+        try{
+            Request request = new Request("usuarios");
+            request.get();
+            if(request.isError()){
+                return new Respuesta(false, request.getError(), "Error al obtener todos los usuarios");
+            }
+            List<UsuarioDTO> result = (List<UsuarioDTO>) request.readEntity(new GenericType<List<UsuarioDTO>>(){});
+            return new Respuesta(true, "Usuarios",result);
+        }catch(Exception ex){
+            return new Respuesta(false, ex.toString(), "No puedo establecerce conexion con el servidor");
+        }
+    }
+    
+    public Respuesta getUserById(Long id){
+        try{
+            Map<String, Object> parametros = new HashMap<>();
+            parametros.put("id", id);
+            Request request = new Request("usuarios", "/{id}", parametros);
+            request.get();
+            if(request.isError()){
+                return new Respuesta(false, request.getError(), "Error al obtener los usuarios");
+            }
+            UsuarioDTO result = (UsuarioDTO) request.readEntity(UsuarioDTO.class);
+            return new Respuesta(true, "Usuario", result);
+        }catch(Exception ex){
+            return new Respuesta(false, ex.toString(), "No puedo establecerce conexion con el servidor");
+        }
+    }
+    
+    public Respuesta getUsersByCedula(String cedula){
+        try{
+            Map<String, Object> parametros = new HashMap<>();
+            parametros.put("term", cedula);
+            Request request = new Request("usuarios/cedula", "/{term}", parametros);
+            request.get();
+            if(request.isError()){
+                return new Respuesta(false, request.getError(), "Error al obtener los usuarios");
+            }
+            List<UsuarioDTO> result = (List<UsuarioDTO>) request.readEntity(new GenericType<List<UsuarioDTO>>(){});
+            return new Respuesta(true, "Usuarios",result);
+        }catch(Exception ex){
+            return new Respuesta(false, ex.toString(), "No puedo establecerce conexion con el servidor");
+        }
+    }
+    
+    public Respuesta getUsersByNombre(String nombre){
+        try{
+            Map<String, Object> parametros = new HashMap<>();
+            parametros.put("term", nombre);
+            Request request = new Request("usuarios/nombre", "/{term}", parametros);
+            request.get();
+            if(request.isError()){
+                return new Respuesta(false, request.getError(), "Error al obtener los usuarios");
+            }
+            List<UsuarioDTO> result = (List<UsuarioDTO>) request.readEntity(new GenericType<List<UsuarioDTO>>(){});
+            return new Respuesta(true, "Usuarios",result);
+        }catch(Exception ex){
+            return new Respuesta(false, ex.toString(), "No puedo establecerce conexion con el servidor");
+        }
+    }
+    
+    public Respuesta getUserByDepartamento(Long id){
+        try{
+            Map<String, Object> parametros = new HashMap<>();
+            parametros.put("id", id);
+            Request request = new Request("usuarios/usuarios_en_departamento", "/{id}", parametros);
+            request.get();
+            if(request.isError()){
+                return new Respuesta(false, request.getError(), "Error al obtener los usuarios");
+            }
+            List<UsuarioDTO> result = (List<UsuarioDTO>) request.readEntity(new GenericType<List<UsuarioDTO>>(){});
+            return new Respuesta(true, "Usuarios", result);
+        }catch(Exception ex){
+            return new Respuesta(false, ex.toString(), "No puedo establecerce conexion con el servidor");
+        }
+    }
+    
+    public Respuesta getJefeDepartamento(Long id){
+        try{
+            Map<String, Object> parametros = new HashMap<>();
+            parametros.put("id", id);
+            Request request = new Request("usuarios/jefe", "/{id}", parametros);
+            request.get();
+            if(request.isError()){
+                return new Respuesta(false, request.getError(), "Error al obtener el jefe del departamento");
+            }
+            UsuarioDTO result = (UsuarioDTO) request.readEntity(UsuarioDTO.class);
+            return new Respuesta(true, "Usuario", result);
+        }catch(Exception ex){
+            return new Respuesta(false, ex.toString(), "No puedo establecerce conexion con el servidor");
+        }
+    }
+    
+    public Respuesta guardarUsuario(UsuarioDTO usuario){
+        try{
+            Request request = new Request("usuarios");
+            request.post(usuario);
+            if(request.isError()){
+                return new Respuesta(false, request.getError(), "No se pudo guardar el usuario");
+            }
+            UsuarioDTO result = (UsuarioDTO) request.readEntity(UsuarioDTO.class);
+            return new Respuesta(true, "Usuario", result);
+        }catch(Exception ex){
+            return new Respuesta(false, ex.toString(), "No puedo establecerce conexion con el servidor");
+        }
+    }
+    
+    public Respuesta modificarUsuario(Long id, UsuarioDTO usuario){
+        try{
+            Map<String, Object> parametros = new HashMap<>();
+            parametros.put("id", id);
+            Request request = new Request("usuarios", "/{id}", parametros);
+            request.put(usuario);
+            if(request.isError()){
+                return new Respuesta(false, request.getError(), "No se pudo modificar el usuario");
+            }
+            UsuarioDTO result = (UsuarioDTO) request.readEntity(UsuarioDTO.class);
+            return new Respuesta(true, "Usuario", result);
+        }catch(Exception ex){
+            return new Respuesta(false, ex.toString(), "No puedo establecerce conexion con el servidor");
+        }
+    }
+    
+    public Respuesta deleteUsuario(Long id){
+        try{
+            Map<String, Object> parametros = new HashMap<>();
+            parametros.put("id", id);
+            Request request = new Request("usuarios", "/{id}", parametros);
+            request.delete();
+            if(request.isError()){
+                return new Respuesta(false, request.getError(), "No se pudo eliminar el usuario");
+            }
+            return new Respuesta(true, "Usuario eliminado exitosamente");
+        }catch(Exception ex){
+            return new Respuesta(false, ex.toString(), "No puedo establecerce conexion con el servidor");
+        }
+    }
+    
+    public Respuesta deleteAll(){
+        try{
+            Request request = new Request("usuarios");
+            request.delete();
+            if(request.isError()){
+                return new Respuesta(false, request.getError(), "Error al eliminar los usuarios");
+            }
+            return new Respuesta(true, "Todos los usuarios fueron eliminados con exito");
+        }catch(Exception ex){
+            return new Respuesta(false, ex.toString(), "No puedo establecerce conexion con el servidor");
+        }
+    }
+}
