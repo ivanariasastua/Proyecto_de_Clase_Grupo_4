e1b7d6d7f5812a5f30938f15d4991d4a5a9fea89
YohellBuzo <54456019+YohellBuzo@users.noreply.github.com>
2020-09-13 20:00:20 -0600

    Vista Cliente

--- api/Tramite/src/main/java/municipales/tramite/PrincipalController.java
+++ api/Tramite/src/main/java/municipales/tramite/PrincipalController.java
@@ -84,4 +84,9 @@ public class PrincipalController implements Initializable {
     private void actUsuarios(ActionEvent event) throws IOException {
         CargarVista("Usuarios");
     }
+    
+    @FXML
+    private void actClientes(ActionEvent event) throws IOException {
+        CargarVista("Clientes");
+    }
 }
